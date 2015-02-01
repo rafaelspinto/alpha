b5e885e6150f7d6467eea8bcb52e478ed745b491
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-01 20:28:36 +0000

    Initial implementation of the Beta class.

---
+++ Beta.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Beta
+ *
+ * @author Rafael Pinto <santospinto.rafael@gmail.com>
+ */
+namespace Alpha;
+
+use Alpha\Http\UriHandler;
+use Alpha\Web\Router;
+
+/**
+ * Class that bootstraps the Alpha Framework.
+ */
+class Beta
+{
+    /**
+     * Handles the request.
+     * 
+     * @return void
+     */
+    public static function run()
+    {
+        $uriHandler = new UriHandler();
+        $uriHandler->setPattern('/{s:controller}/{s:action}');
+        $router = new Router($uriHandler, PATH_CONTROLLER);
+        $router->go($_SERVER['REQUEST_URI']);        
+    }
+}
+
+
