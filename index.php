6b160fb6b45c3ba08305d83f5405e11abf7cdefa
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-03 00:59:12 +0000

    Minor standardization.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 define('PATH_ROOT', __DIR__);
 define('PATH_CONTROLLER', __DIR__ . DIRECTORY_SEPARATOR . 'webapp' . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR);
 
-require_once __DIR__. DIRECTORY_SEPARATOR . 'alpha/core' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+require_once __DIR__. DIRECTORY_SEPARATOR . 'alpha' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'Autoloader.php';
 
 try {
     \Alpha\Beta::gamma();
