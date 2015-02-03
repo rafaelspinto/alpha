3f5f1b40d6bcc7d9db03b9cbaa9b9433f8374ce6
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-03 00:54:24 +0000

    Added more examples to ExampleController.

--- webapp/controller/ExampleController.php
+++ webapp/controller/ExampleController.php
@@ -3,12 +3,22 @@ class ExampleController
 {       
     public function getSearch($QUERY_name)
     {
-        print 'You searched for : '.$QUERY_name;
+        print sprintf('You searched for "%s".', $QUERY_name);
     }
     
     public function getEdit($PATH_id)
     {
-        print 'You are editing id : '. $PATH_id;
+        print sprintf('You are editing id "%s".', $PATH_id);
+    }
+    
+    public function postEdit($PATH_id, $PARAM_name)
+    {
+        print sprintf('You are submitting edit id "%d" with name "%s".', $PATH_id, $PARAM_name);
+    }
+    
+    public function getInfo($COOKIE_var1, $SESSION_name)
+    {
+        print sprintf('You accessing info with the COOKIE var1 with the value "%s" and SESSION name with value "%s".', $COOKIE_var1, $SESSION_name);
     }
 }
 
