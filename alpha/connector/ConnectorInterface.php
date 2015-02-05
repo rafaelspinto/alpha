c52608bdfd03e091eaac1b0b5f8ee286dd39ef5e
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-05 09:20:08 +0000

    Implemented the ConnectorInterface.

---
+++ alpha/connector/ConnectorInterface.php
@@ -0,0 +1,22 @@
+<?php                                                                                                                                                                                                                                                                          
+/**                                                                                                                                                                                                                                                                            
+ * ConnectorInterface
+ *                                                                                                                                                                                                                                                                             
+ * @author Rafael Pinto <santospinto.rafael@gmail.com>                                                                                                                                                                                                                         
+ */                                                                                                                                                                                                                                                                            
+namespace Alpha\Connector;
+
+/**
+ * Defines the interface that the connectors must implement.
+ */
+interface ConnectorInterface
+{
+	/**
+     * Sets the configuration.
+     * 
+	 * @param array $configuration The array containing the connector configuration.
+     * 
+     * @return void
+	 */	
+	function setup(array $configuration);
+}
