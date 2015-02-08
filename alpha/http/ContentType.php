e4bf92af88b396bf2618acf217e6a10603efc559
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-08 00:21:23 +0000

    Implemented http/ContentType and http/StatusCode.

---
+++ alpha/http/ContentType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ContentType
+ *
+ * @author Rafael Pinto <santospinto.rafael@gmail.com>
+ */
+namespace Alpha\Http;
+
+/**
+ * Class that contains the types of contents.
+ */
+class ContentType
+{
+    const TEXT_HTML = 'text/html';
+}
---
+++ alpha/http/StatusCode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * StatusCode
+ *
+ * @author Rafael Pinto <santospinto.rafael@gmail.com>
+ */
+namespace Alpha\Http;
+
+/**
+ * Class that contains the status codes.
+ */
+class StatusCode
+{
+    const OK = 200;
+}
