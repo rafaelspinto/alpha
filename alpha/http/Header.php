8797cbf751fd9ab5c85cf00d5bdfe88034445e64
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-10 22:22:35 +0000

    Added Header.

---
+++ alpha/http/Header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Header
+ *
+ * @author Rafael Pinto <santospinto.rafael@gmail.com>
+ */
+namespace Alpha\Http;
+
+/**
+ * Class that contains the headers.
+ */
+class Header
+{
+    const LOCATION = 'Location';
+}
+
