7f4fc9ce70509d1a10216a855a5a51202006ac00
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-11 00:32:08 +0000

    Implemented first version of CrudBaseController and changed sample UserController to use it.

--- samples/controller/UserController.php
+++ samples/controller/UserController.php
@@ -1,41 +1,13 @@
 <?php
-use Webapp\Model\User;
-
-class UserController extends \Alpha\Web\ControllerAbstract
+class UserController extends \Alpha\Web\CrudBaseController
 {
-    public function get()
+    public function __construct(\Alpha\Http\UriHandler $uriHandler)
     {
-        $this->data['users'] = User::find();
+        parent::__construct($uriHandler, "Webapp\Model\User", 'users', 'user');
     }
     
     public function postSearch($PARAM_name)
     {
-        $this->data['users'] = User::find(array('name' => $PARAM_name));
-    }
-    
-    public function getEdit($PATH_id)
-    {
-        $this->data['user'] = User::findByKey($PATH_id);
-    }
-    
-    public function getDelete($PATH_id)
-    {
-        $this->data['user'] = User::findByKey($PATH_id);
-    }
-    
-    public function postDelete($PATH_id)
-    {
-        $this->data['user'] = User::delete($PATH_id);
-    }
-    
-    public function postEdit($PATH_id, $PARAM_name)
-    {
-        $this->data['user'] = User::update(array('id' => $PATH_id, 'name' => $PARAM_name));
-        $this->redirectTo('/User');
-    }
-    
-    public function postCreate($PARAM_name)
-    {
-        $this->data['user'] = User::create(array('name' => $PARAM_name));
+        $this->data['users'] = Webapp\Model\User::find(array('name' => $PARAM_name));
     }
 }
