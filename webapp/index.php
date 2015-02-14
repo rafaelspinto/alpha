1d2928fc08371101f9060c181c675690a7d7780b
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-14 13:46:47 +0000

    Added PATH_MODEL to index.php.

--- webapp/index.php
+++ webapp/index.php
@@ -8,6 +8,7 @@ define('PATH_ROOT', dirname(__DIR__));
 define('PATH_PROJECT', __DIR__ . DIRECTORY_SEPARATOR);
 define('PATH_CONTROLLER', PATH_PROJECT . 'controller' . DIRECTORY_SEPARATOR);
 define('PATH_VIEW', PATH_PROJECT . 'view' . DIRECTORY_SEPARATOR);
+define('PATH_MODEL', PATH_PROJECT . 'model' . DIRECTORY_SEPARATOR);
 require_once PATH_ROOT .DIRECTORY_SEPARATOR . 'alpha' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'Autoloader.php';
 
 ini_set('error_reporting', E_ALL);
