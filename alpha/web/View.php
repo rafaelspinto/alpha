c11c367bb4a7843cd26f9d0e7e39aa28fabb90d3
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-15 14:16:31 +0000

    Implemented the ViewTag mechanism to allow new tags to be adde in a simpler way.

--- alpha/web/View.php
+++ alpha/web/View.php
@@ -7,16 +7,41 @@
 namespace Alpha\Web;
 
 use Alpha\Connector\ViewConnectorInterface;
+use Alpha\Web\View\ViewTagAbstract;
+use Alpha\Web\View\PropertiesTag;
+use Alpha\Web\View\ForeachTag;
+use Alpha\Web\View\LayoutTag;
 
 /**
  * Base class for Views.
  */
 class View implements ViewConnectorInterface
 {
-    const REGEX_DATA   = '#@{(?!/?foreach|layout)(.*?)}#';
-    const REGEX_LOOP   = '#@{foreach (.*?)}(.*)@{/foreach \1}#si';
-    const REGEX_LAYOUT = '#@{layout "(.*?)"}#';
+    protected $tags;
     
+    /**
+     * Constructs a View.
+     */
+    public function __construct()
+    {
+        $this->tags = array();
+        $this->registerTag(new LayoutTag());
+        $this->registerTag(new ForeachTag());
+        $this->registerTag(new PropertiesTag());
+    }
+
+    /**
+     * Registers a tag in the View.
+     * 
+     * @param \Alpha\Web\View\ViewTagAbstract $tag The tag to be registered.
+     * 
+     * @return void
+     */
+    public function registerTag(ViewTagAbstract $tag)
+    {
+        $this->tags[] = $tag;
+    }
+        
     /**
      * Renders the content of the view with the data.
      * 
@@ -27,9 +52,9 @@ class View implements ViewConnectorInterface
      */
     public function render($content, array $data)
     {
-        $this->handleLayouts($content);
-        $this->handleLoops($content, $data);
-        $this->handleDataProperties($content, $data);
+        foreach ($this->tags as $tag){            
+            $content = $tag->render($content, $data);
+        }
         return $content;
     }
     
@@ -43,100 +68,5 @@ class View implements ViewConnectorInterface
     public function setup(array $configuration)
     {
         // void
-    }
-    
-    /**
-     * Returns the value of the placeholder by providing the path(keys) and the array.
-     * 
-     * @param array $keys The array containing the keys that identify the path.
-     * @param array $data The array containing the data.
-     * 
-     * @return string
-     */
-    protected function getValue($keys, $data)
-    {
-        if(count($keys) > 1 && isset($data[$keys[0]])) {
-            return $this->getValue(array_slice($keys, 1), $data[$keys[0]]);
-        }
-        return isset($data[$keys[0]]) ? $data[$keys[0]] : '';
-    }
-    
-    /**
-     * Includes the content of the LAYOUTS.
-     * 
-     * @param string $content The original content.
-     * 
-     * @return string
-     */
-    protected function handleLayouts(&$content)
-    {
-        $matches = array();
-        $found   = preg_match_all(static::REGEX_LAYOUT, $content, $matches);
-        if($found) {
-            for($i=0;$i<$found;$i++) {
-                $viewFile = PATH_VIEW . $matches[1][$i];
-                if(file_exists($viewFile)) {
-                    $replaceContent = file_get_contents($viewFile);
-                }else {
-                    $replaceContent = 'layout_not_found:'.$viewFile;
-                }
-                $content = str_replace($matches[0][$i], $replaceContent, $content);
-            }
-        }
-        return $content;
-    }
-    
-    /**
-     * Renders the content of the FOREACH with the data.
-     * 
-     * @param string $content The original content.
-     * @param array  $data    The data to bind.
-     * 
-     * @return string
-     */
-    protected function handleLoops(&$content, &$data)
-    {
-        $matches = array();
-        $found   = preg_match_all(static::REGEX_LOOP, $content, $matches);
-        if($found) {
-            $innerContent = '';
-            for($i=0;$i<$found;$i++) {
-                $list = $this->getValue(explode('.', $matches[1][$i]), $data);
-                if(!empty($list)){
-                    foreach($list as $item) {
-                        $innerContent .= $this->render($matches[2][$i], $item);
-                    }
-                }
-                $content = str_replace($matches[0][$i], $innerContent, $content);
-            }
-        }
-        return $content;
-    }
-    
-    /**
-     * Renders the content of the view with the data.
-     * 
-     * @param string $content The original content.
-     * @param array  $data    The data to bind.
-     * 
-     * @return string
-     */
-    protected function handleDataProperties(&$content, &$data)
-    {
-        $matches = array();
-        $found   = preg_match_all(static::REGEX_DATA, $content, $matches);
-        if($found) {
-            for($i=0; $i < $found; $i++) {
-                $tmpData = explode('.', $matches[1][$i]);
-                $count   = count($tmpData); 
-                if($count > 1) {                    
-                    $value = $this->getValue($tmpData, $data);
-                }else{
-                    $value = isset($data[$matches[1][$i]]) ? $data[$matches[1][$i]] : null;
-                }
-                $content = str_replace($matches[0][$i], $value, $content);
-            }
-        }
-        return $content;
-    }
+    }         
 }
---
+++ alpha/web/view/LayoutTag.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * LayoutTag
+ *
+ * @author Rafael Pinto <santospinto.rafael@gmail.com>
+ */
+namespace Alpha\Web\View;
+
+use Alpha\Web\View\ViewTagAbstract;
+
+/**
+ * Handles Layout tags.
+ */
+class LayoutTag extends ViewTagAbstract
+{
+    /**
+     * Constructs a LayoutTag.
+     */
+    public function __construct()
+    {
+        parent::__construct('#@{layout "(.*?)"}#');
+    }
+    
+    /**
+     * Handles the matches found.
+     * 
+     * @param string $content The original content.
+     * @param array  $data    The data to bind.
+     * @param array  $matches The matches found.
+     * @param int    $found   The number of matches found.
+     * 
+     * @return string
+     */
+    protected function handleMatches($content, array $data, array $matches, $found)
+    {
+        for ($i = 0; $i < $found; $i++) {
+            $viewFile = PATH_VIEW . $matches[1][$i];
+            if (file_exists($viewFile)) {
+                $replaceContent = file_get_contents($viewFile);
+            } else {
+                $replaceContent = 'layout_not_found:' . $viewFile;
+            }
+            $content = str_replace($matches[0][$i], $replaceContent, $content);
+        }
+        return $content;
+    }
+}
