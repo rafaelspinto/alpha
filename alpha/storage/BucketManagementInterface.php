1e84eb49b05882fadc7ec0496e8a8701d25902c9
Rafael Pinto <santospinto.rafael@gmail.com>
2015-02-23 22:22:46 +0000

    Defined the interface for Bucket Management.

---
+++ alpha/storage/BucketManagementInterface.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * BucketManagementInterface
+ *
+ * @author Rafael Pinto <santospinto.rafael@gmail.com>
+ */
+namespace Alpha\Storage;
+
+use Alpha\Connector\RepositoryInterface;
+
+/**
+ * Defines the interface of the operations from Bucket to Repository.
+ */
+interface BucketManagementInterface
+{
+    /**
+     * Called when the Bucket is being created.
+     * 
+     * @param \Alpha\Connector\RepositoryInterface $repo The repository.
+     * 
+     * @return void
+     */
+    function onCreate(RepositoryInterface $repo);
+    
+    /**
+     * Called when the Bucket is being destroy.
+     * 
+     * @param \Alpha\Connector\RepositoryInterface $repo The repository.
+     * 
+     * @return void
+     */
+    function onDestroy(RepositoryInterface $repo);
+    
+    /**
+     * Called when the Bucket is being upgraded.
+     * 
+     * @param \Alpha\Connector\RepositoryInterface $repo       The repository.
+     * @param int                                  $oldVersion The id of the old version.
+     * @param int                                  $newVersion The id of the new version.
+     * 
+     * @return void
+     */
+    function onUpgrade(RepositoryInterface $repo, $oldVersion, $newVersion);
+    
+    /**
+     * Called when the Bucket is being downgraded.
+     * 
+     * @param \Alpha\Connector\RepositoryInterface $repo       The repository.
+     * @param int                                  $oldVersion The id of the old version.
+     * @param int                                  $newVersion The id of the new version.
+     * 
+     * @return void
+     */
+    function onDowngrade(RepositoryInterface $repo, $oldVersion, $newVersion);
+}
